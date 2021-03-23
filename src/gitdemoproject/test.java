37684a1b1a525b90fc87a9dd10ea878e76e70d8e
samrat <chysam@ymail.com>
2021-03-23 00:19:46 -0600

    first commit

---
+++ src/gitdemoproject/test.java
@@ -0,0 +1,20 @@
+package gitdemoproject;
+
+public class test {
+
+	public static void main(String[] args) {
+
+
+		
+	int val=10;
+	for(int i=2;i<val/2;i++)
+	{
+		if(val%i==0)
+		{
+			System.out.println("this is not a prime number");
+		}
+	}
+
+	}
+
+}
